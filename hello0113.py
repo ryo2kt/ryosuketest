2c7394ca9ea82d9cc839f1a0292a5588b93fac1d
Ryosuke Takemoto <ryosuketakemoto@RyosukenoMacBook-Air.local>
2022-01-13 11:35:00 +0900

    Added new file

---
+++ hello0113.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def returnList(oroginalList):
+	finalList=[]
+	for data in oroginalList:
+		finalList.append(data%3)
+	return finalList
+
+test=returnList([2,6,4,1,3,5])
+print(test)
